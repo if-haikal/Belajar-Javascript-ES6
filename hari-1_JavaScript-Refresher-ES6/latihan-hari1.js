f1f279b755c809a2c86c80d41dc4b3bd1d44bdef
if-haikal <fasalalifhaikalirawan@gmail.com>
2025-06-20 02:05:49 +0700

    Done Javascript Refresher ES6

--- hari-1_JavaScript-Refresher-ES6/latihan-hari1.js
+++ hari-1_JavaScript-Refresher-ES6/latihan-hari1.js
@@ -139,8 +139,8 @@
 
 // document.getElementById("car2").innerHTML = "Mobil pertama adalah " + mustang.name + " dengan warna " + mustang.color + " dan mobil kedua adalah " + BMW.name + " dengan warna " + BMW.color + ".";
 
-let manusia = "";
 
+// *Object Constructor Function* - Human
 const human = (Firstname, Lastname, Age, Height, Weight) => {
   let BMI = Weight / ((Height / 100) * (Height / 100));
 
@@ -172,15 +172,121 @@ const human = (Firstname, Lastname, Age, Height, Weight) => {
 
   return manusia;
 }
+let waktu = 5;
+let vian = human("Vian", "Aprilianto", 21, 150, 60);
+let apri = human("Apri", "Wijayanto", 23, 180, 80);
 
 setTimeout(() => {
     vian.height = 172;
     vian.weight = 80;
     document.getElementById("demo").innerHTML = vian.sayHello() + " and " + vian.myBMI() + ", " + vian.cekBMI() + ". <br> And now my height is " + vian.height + " cm and my weight is " + vian.weight + " kg.";
-  }, 5000);
+  }, 6000);
 
+let hitungMundur = setInterval(() => {
+  document.getElementById('sub').innerHTML = "<i>Hitung mundur " + waktu + " detik lagi untuk mengubah data Vian.</i>";
 
-let vian = human("Vian", "Aprilianto", 21, 150, 60);
-let apri = human("Apri", "Wijayanto", 23, 180, 80);
+  let cekWaktu = (waktu <= 0) ? 
+    (clearInterval(hitungMundur), document.getElementById('sub').innerHTML = "<i>Waktu habis, data Vian telah berubah!</i>") : 
+    waktu--;
+
+  return cekWaktu;
+}, 1000);
 
 document.getElementById("demo").innerHTML = vian.sayHello() + " and " + vian.myBMI() + ", " + vian.cekBMI();
+
+
+
+
+
+
+// // Map
+let angka = [1, 2, 3, 4, 5];
+let angkaKuadrat = angka.map((x) => x * x);
+
+document.getElementById("car").innerHTML = "Angka awal-nya adalah " + angka + " dan hasil kuadrat-nya adalah " + angkaKuadrat + ".";
+
+// // // Map + ternary operation
+let angkaTernary = angka.map((x) => (x % 2 === 0) ? x * x : x * 3);
+
+document.getElementById("car2").innerHTML = "Angka awal-nya adalah " + angka + " dan hasil modulus menggunakan Ternary adalah " + angkaTernary + ".";
+
+const fruits = new Map([
+  ["apple", 5000, "avaible"],
+  ["banana", 2000, "notAvaible"],
+  ["orange", 3000, "notAvaible"],
+  ["grape", 4000, "avaible"]
+]);
+let numb = fruits.get("banana");
+console.log("Harga buah pisang adalah " + numb + " rupiah.");
+
+
+// // Filter
+let angkaFilter = angka.filter((x) => x % 2 === 0)
+console.log("Angka awal-nya adalah " + angka + " dan hasil filter-nya adalah " + angkaFilter + ".");
+
+let objek = [
+  { name: "Vian", age: 21 },
+  { name: "Apri", age: 23 },
+  { name: "Budi", age: 18 },
+  { name: "Siti", age: 25 }
+]
+// let filterObjek = objek.filter((x) => x.age >= 21).map((x) => x.name).join(", ");
+
+// console.log("Objek awal-nya adalah " + JSON.stringify(objek) + " dan hasil filter-nya untuk usia 21++ adalah " + JSON.stringify(filterObjek) + "."); 
+
+const eObjek = objek.filter(obj => obj.age >= 21).map(obj => obj.name).join(", ");
+
+console.log("Objek awal-nya adalah " + JSON.stringify(objek) + " dan hasil filter-nya untuk usia 21++ adalah " + eObjek + ".")
+
+
+
+
+
+
+// Spread Operator
+let angkaAwal = [1, 2, 3, 4, 5];
+ 
+const penggabunganAngka = (angkaAwal) => {
+  return console.log([...angkaAwal, 6, 7, 8, 9, 10]);
+}
+
+// Spread Operator dan Destructuring
+let [ angkaPertama, angkaKedua, angkaKetiga, ...angkaLainnya ] = angkaAwal;
+console.log("Angka pertama adalah " + angkaPertama + " dan angka kedua adalah " + angkaKedua + " dan angka kedua adalah " + angkaKetiga + ". Angka lainnya adalah " + angkaLainnya + ".");
+
+
+
+
+
+
+
+// // Destructuring
+let fruit = {
+  name : "Apple",
+  color: "Red",
+  price: 5000,
+  isAvailable: true
+}
+
+let car = {
+  name: "Toyota",
+  model: "Avanza",
+  year: 2020,
+  price: 200000000,
+  isElectric: false
+}
+
+const concatenateObj = (obj1, obj2) => {
+  return { ...obj1, ...obj2}
+}
+const concatenateObj2 = concatenateObj(fruit, car);
+console.log("Hasil penggabungan objek fruit dan car adalah: ", concatenateObj2);
+
+
+// // // Destructuring dengan default value
+let { name, price, isAvailable, from = "Bali" } = fruit;
+console.log("Tersedia buah " + fruit.name + " dengan harga " + fruit.price + " dan " + (fruit.isAvailable ? "tersedia" : "tidak tersedia") + " di toko."); 
+
+// // // Destructuring dengan alias
+let { name: fruitName, color: fruitColor } = fruit;
+console.log("Nama buahnya adalah " + fruitName + " dan warnanya adalah " + fruitColor + ".");
