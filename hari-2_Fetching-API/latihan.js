3354a598011db3d93eb096f364c32f48c3cc11fb
if-haikal <fasalalifhaikalirawan@gmail.com>
2025-06-21 22:06:34 +0700

    setelah menggunakan tailwind CSS

--- hari-2_Fetching-API/latihan.js
+++ hari-2_Fetching-API/latihan.js
@@ -57,12 +57,13 @@ const containerDisplay = document.getElementById("card-display");
 const fetchData = async () => {
     try {
         const api = await fetch(endpoint);
-        const data = await api.json();
 
         // cek response dari API
         if (!api.ok) {
             throw new Error("API tidak merespon dengan baik");
         }
+
+        const data = await api.json();
 
         // Cek apakah terdapat data dan ada element pertama
         if (data.length === 0 || !data[0]) {
@@ -71,18 +72,25 @@ const fetchData = async () => {
         }
 
         // Tampilkan data pada containerDisplay
-        const {category, title, price, image} = await data[0];
+        data.forEach(({category, title, price, image}) => {
+            const card = document.createElement("div");
+            card.className = "mb-4 p-4 break-inside-avoid bg-white shadow-md rounded-lg border";
 
-        containerDisplay.innerHTML += `
-            <img src="${image}" alt="${title}" height="100px" width="100px">
-            <h3>${title}</h3>
-            <p>${category}</p>
-            <p>Price: ${price}</p>
-        `;
+            card.innerHTML = `
+                <img src="${image}" alt="${title}" class="h-40 w-full object-contain">
+                <h3 class="font-semibold text-lg md:text-center">${title}</h3>
+                <p class="text-sm text-gray-500 md:text-center">${category}</p>
+                <p class="text-sm text-gray-700 md:text-center pb-10">Price: $${price}</p>
+            `;
+
+            containerDisplay.appendChild(card)
+        });
 
     } catch (error) {
         console.error("Fetch error : ", error);
-        containerDisplay.innerHTML = "<p>Gagal buat fetching data API</p>";
+        containerDisplay.innerHTML = `<p class="text-md text-red-500 text-center border-2 border-solid border-red-500">Gagal buat fetching data API</p>`;
     }
 }
-fetchData()
+
+
+fetchData();
